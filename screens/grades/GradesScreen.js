41a881512449ac7ad3298e4abfd4af2ca9694e54
Andres Miranda <andres.miranda.dev@gmail.com>
2023-08-16 01:05:20 +0900

    added grades details screen with animations

--- screens/grades/GradesScreen.js
+++ screens/grades/GradesScreen.js
@@ -1,34 +1,77 @@
 import { FlatList, StyleSheet, View } from 'react-native';
 import React from 'react';
+import { LinearGradient } from 'expo-linear-gradient';
+
 import Screen from '../../components/Screen';
-import AppText from '../../components/AppText';
 import SubjectListItem from '../../components/gradesscreen/SubjectListItem';
-import appStyles from '../../config/styles';
-import ListItemSeparator from '../../components/ListItemSeparator';
+import colors from '../../config/colors';
 
 const grades = [
-  { id: 1, name: 'Matematicas', grades: ['5.6', '5,0', '6,4', '4,6'] },
-  { id: 2, name: 'Lenguaje', grades: ['5.2', '4,0', '5,4', '6,6'] },
+  {
+    id: 1,
+    name: 'Matematicas',
+    grades: ['5.6', '5,0', '6,4', '4,6'],
+    lottie: require('../../assets/animations/math.json'),
+  },
+  {
+    id: 2,
+    name: 'Lenguaje',
+    grades: ['5.2', '4,0', '5,4', '6,6'],
+    lottie: require('../../assets/animations/lenguage.json'),
+  },
   {
     id: 3,
     name: 'Historia y Ciencias Sociales',
     grades: ['4.7', '4,0', '5,4', '4,6'],
+    lottie: require('../../assets/animations/history.json'),
   },
-  { id: 4, name: 'Biologia', grades: ['6.9', '6,0', '6,4', '6,6'] },
-  { id: 5, name: 'Fisica', grades: ['4.9', '7,0', '7,4', '4,6'] },
-  { id: 6, name: 'Quimica', grades: ['4.9', '4,0', '4,4', '6,6'] },
-  { id: 7, name: 'Educacion Fisica', grades: ['6.9', '7,0', '7,0', '7,0'] },
-  { id: 8, name: 'Musica', grades: ['6.9', '7,0', '5,9', '5,7'] },
+  {
+    id: 4,
+    name: 'Biologia',
+    grades: ['6.9', '6,0', '6,4', '6,6'],
+    lottie: require('../../assets/animations/biology.json'),
+  },
+  {
+    id: 5,
+    name: 'Fisica',
+    grades: ['4.9', '7,0', '7,4', '4,6'],
+    lottie: require('../../assets/animations/physics.json'),
+  },
+  {
+    id: 6,
+    name: 'Quimica',
+    grades: ['4.9', '4,0', '4,4', '6,6'],
+    lottie: require('../../assets/animations/chemistry.json'),
+  },
+  {
+    id: 7,
+    name: 'Educacion Fisica',
+    grades: ['6.9', '7,0', '7,0', '7,0'],
+    lottie: require('../../assets/animations/sports.json'),
+  },
+  {
+    id: 8,
+    name: 'Musica',
+    grades: ['6.9', '7,0', '5,9', '5,7'],
+    lottie: require('../../assets/animations/music.json'),
+  },
   {
     id: 9,
     name: 'Idioma Extranjero Ingles',
     grades: ['5,4', '6,7', '5,5', '5,4'],
+    lottie: require('../../assets/animations/english.json'),
   },
 ];
 
 export default function GradesScreen({ navigation }) {
   return (
     <Screen>
+      <LinearGradient
+        colors={[colors.secondary, colors.light]}
+        start={{ x: 1, y: 1.2 }}
+        end={{ x: 0.9, y: 0.6 }}
+        style={styles.background}
+      />
       <View style={styles.container}>
         <View style={styles.listContainer}>
           <FlatList
@@ -50,6 +93,13 @@ export default function GradesScreen({ navigation }) {
 }
 
 const styles = StyleSheet.create({
+  background: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
--- screens/grades/SubjectDetailsScreen.js
+++ screens/grades/SubjectDetailsScreen.js
@@ -1,5 +1,9 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import LottieView from 'lottie-react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
 import Screen from '../../components/Screen';
 import AppText from '../../components/AppText';
 import colors from '../../config/colors';
@@ -7,11 +11,49 @@ import colors from '../../config/colors';
 export default function SubjectDetailsScreen({ route }) {
   const subject = route.params;
 
+  const totalSum = subject.grades.reduce((accumulator, grade) => {
+    return parseFloat(grade) + accumulator;
+  }, 0);
+
+  const average = Number((totalSum / subject.grades.length).toFixed(1));
+
   return (
     <Screen>
+      <LinearGradient
+        colors={[colors.secondary, colors.light]}
+        start={{ x: 1, y: 1.2 }}
+        end={{ x: 0.9, y: 0.6 }}
+        style={styles.background}
+      />
       <View style={styles.container}>
         <View style={styles.titleContainer}>
           <AppText style={styles.title}>{subject.name}</AppText>
+        </View>
+        <View style={styles.animation}>
+          <LottieView
+            source={subject['lottie']}
+            loop
+            autoPlay
+            resizeMode="contain"
+          />
+        </View>
+        <View style={styles.averageContainer}>
+          <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+            <MaterialCommunityIcons
+              name="star"
+              size={40}
+              color={colors.yellow}
+            />
+            <AppText style={styles.averageText}>Promedio</AppText>
+          </View>
+          <AppText
+            style={[
+              styles.average,
+              { color: average < 4 ? colors.danger : colors.primary },
+            ]}
+          >
+            {average}
+          </AppText>
         </View>
       </View>
     </Screen>
@@ -19,13 +61,36 @@ export default function SubjectDetailsScreen({ route }) {
 }
 
 const styles = StyleSheet.create({
+  animation: {
+    height: 200,
+    width: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  average: {
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+  averageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  background: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+  },
   container: {
     flex: 1,
+    alignItems: 'center',
   },
   title: {
     fontSize: 30,
     fontWeight: 'bold',
     color: colors.primary,
+    margin: 10,
   },
   titleContainer: {
     justifyContent: 'center',
